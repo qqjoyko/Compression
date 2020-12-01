b2d06bc99efb9a7af00e65d2bdc4334896ebb3c3
KangKaneYang <42194806+KangKaneYang@users.noreply.github.com>
2020-11-30 23:16:06 -0500

    Update HuffmanCode.java

--- compression/src/HuffmanCode.java
+++ compression/src/HuffmanCode.java
@@ -41,4 +41,25 @@ public class HuffmanCode {
     
     
 }
+         /**
+       * 
+       * @param roots
+       *            initial root of each tree
+       * @return root of huffman tree
+       */
+      public BinaryNode buildHuffmanTree(List<BinaryNode> roots) {
+          if (roots.size() == 1)// 只有一个结点
+              return roots.remove(0);
+          PriorityQueue<BinaryNode> pq = new PriorityQueue<BinaryNode>(roots);//优先级队列保存所有叶子结点
+          while (pq.size() != 1) {
+              BinaryNode left = pq.remove();//频率最小的先出队列
+              BinaryNode right = pq.remove();
+              BinaryNode parent = new BinaryNode(
+                      left.frequency + right.frequency, left, right, null);//构造父结点
+              left.parent = parent;
+              right.parent = parent;
+              pq.add(parent);//新构造好的根结点插入到优先级队列中
+          }
+          return (root = pq.remove());
+      }
 }
